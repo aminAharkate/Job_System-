598248d3550039dbdce3e089ae7c66cb79b22d36
Amine Aharkate <amineaharkate@gmail.com>
2026-06-16 15:11:40 +0100

    feat: enable Eureka server and service registry (localhost:8761)

---
+++ job_microservices_arch/services-registration/src/main/java/com/amine/services/registration/ServicesRegistrationApplication.java
@@ -0,0 +1,15 @@
+package com.amine.services.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServicesRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesRegistrationApplication.class, args);
+	}
+
+}
---
+++ job_microservices_arch/services-registration/src/test/java/com/amine/services/registration/ServicesRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amine.services.registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
